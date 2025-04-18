0e0459f0756d888559988fcbaa8905a42ae04a7f
angalakuditiparameswari <angalakuditiparameswari@gmail.com>
2025-04-18 21:12:03 +0530

    initial commit

---
+++ src/main/java/com/example/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,26 @@
+package com.example.inventoryservice.controller;
+
+import com.example.inventoryservice.dto.InventoryResponse;
+import com.example.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
+
---
+++ src/main/java/com/example/inventoryservice/service/InventoryService.java
@@ -0,0 +1,24 @@
+package com.example.inventoryservice.service;
+
+import com.example.inventoryservice.dto.InventoryResponse;
+import com.example.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
